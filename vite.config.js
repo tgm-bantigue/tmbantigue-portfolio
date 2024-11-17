597145b9b1b1c7c323dfe88845f2af4bb25be9c3
tgm-bantigue <your.email@example.com>
2024-11-18 06:44:08 +0800

    Normalize line endings

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/tmbantigue-portfolio/', // Set this to your repository name for GitHub Pages
+  base: '/tmbantigue-portfolio/',
   plugins: [react()],
   assetsInclude: ['**/*.{png,jpg,jpeg,JPG,PNG}'],
   server: {
     port: 3000, 
     fs: {
-      allow: ['..'], // This is to allow file access outside of the project directory
+      allow: ['..'],
     }
   }
 });
